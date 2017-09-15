d4cf8ed2de93afdd43a8bc7f1768e9afa19b4534
Rakowska <Malgorzata.Rakowska@ge.com>
2017-09-15 13:55:53 +0200

    manage course

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -32,5 +32,8 @@ export default {
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
     ]
+  },
+  alias: {
+    '@': './src'
   }
 };
